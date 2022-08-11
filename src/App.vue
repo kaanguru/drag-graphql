8d41f703418b903072d5b06dd683d8499b0e8fe6
kaanguru <kaanguru@mail.ru>
2022-08-11 20:45:44 +0300

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{ hello }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import gql from 'graphql-tag'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    apollo: {
+        // Simple query that will update the 'hello' vue property
+        hello: gql`query {
+      hello
+    }`,
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
